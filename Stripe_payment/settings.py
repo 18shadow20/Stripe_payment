5684d08fffcea0dcb2d943aa876cf24e683fad76
vitalya <127036181+18shadow20@users.noreply.github.com>
2025-11-19 03:49:16 +0300

    Update settings.py

--- Stripe_payment/settings.py
+++ Stripe_payment/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-@jjn+wy90x%!@g*h^4d@e4p^qdq@wo=vyv&gmqo)dmbkpn@cli
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
